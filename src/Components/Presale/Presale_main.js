528940f8a8cf072d606f0f1aeb8ba60dcadb095d
draco <122390064+Darco786@users.noreply.github.com>
2023-01-17 00:22:54 +0530

    new

--- src/Components/Presale/Presale_main.js
+++ src/Components/Presale/Presale_main.js
@@ -12,26 +12,21 @@ function Presale_main() {
             <div className="fill-bar">
 
             </div>
-            {/* <img src="https://ik.imagekit.io/cforcrypto/fire.webp" alt="" className="fire-1" />
-            <img src="https://ik.imagekit.io/cforcrypto/Group_64.png" alt="" className="bullet-1" /> */}
+            <img src="https://ik.imagekit.io/cforcrypto/fire.webp" alt="" className="fire-1" />
+            <img src="https://ik.imagekit.io/cforcrypto/Group_64.png" alt="" className="bullet-1" />
           </div>
+          <p className='bar-info'>progress(<span className='green'> $0</span> / $700,000 )</p>
           <div className="pre-box-1">
-
             <div className="pre-box-2">
-
               <div className="head-title text-center">
                 <h3>PreSale Sale</h3>
-
                 <span className="span-btn">$250 min / $20,000 max</span>
-
                 <p>Deelance Official Contracts</p>
               </div>
               <div className="price-box text-center">
                 <h1>
                   $ 0.018 <span className="sp-white">/$DLANCE</span>
                 </h1>
-                
-                <p>Raise $230 out of $540,000 / 15,000 Sold out of 30,000,000</p>
               </div>
 
               <div className="text-center align-items-center d-flex jsa ">
@@ -43,7 +38,7 @@ function Presale_main() {
             </div>
           
           </div>
-          <p className='white'>24 HOURS, 38 MINUTES, 51 SECONDS Remaining</p>
+          <p className='white'><span className='green'>24</span> HOURS, <span className='green'>38</span> MINUTES, <span className='green'>0</span> SECONDS Remaining</p>
         </div>
        
      
