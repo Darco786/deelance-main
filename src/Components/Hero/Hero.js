d62a43df5df2fcf3e3e2a10754c4d5b92cbeaea8
draco <122390064+Darco786@users.noreply.github.com>
2023-01-17 01:34:00 +0530

    new

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import "./Hero.css";
-
+import {
+  FaTwitter,
+  FaFacebookSquare,
+  FaMedium,
+  FaTelegramPlane,
+  FaLinkedin,
+  FaDiscord,
+  FaInstagram,
+} from "react-icons/fa";
 import {AiOutlineArrowRight} from 'react-icons/ai'
 import Model from "Components/Popup/Model";
 import Lottie from 'react-lottie';
@@ -37,16 +45,64 @@ function Hero() {
               
             </div>
             <div className="col-md-6 text-center align-items-center">
-              {/* <img
-                className="hero-img"
-                src={Heroe}
-                alt=""
-              /> */}
+            
               <Lottie 
 	    options={defaultOptions}
         className='loti-1'
       />
+       <div className="hero-ss">
+                <a
+                  href="https://twitter.com/deelance_com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaTwitter />
+                </a>
+                <a
+                  href="https://www.facebook.com/profile.php?id=100087266006183"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaFacebookSquare />
+                </a>
+                <a
+                  href="https://medium.com/@deeLance"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaMedium />
+                </a>
+                <a
+                  href="https://t.me/deelancer"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaTelegramPlane />
+                </a>
+                <a
+                  href="https://www.linkedin.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaLinkedin />
+                </a>
+                <a
+                  href="https://discord.gg/vhH3Sbt9NQ"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaDiscord />
+                </a>
+                <a
+                  href="https://www.instagram.com/deelanceofficial/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaInstagram />
+                </a>
+              </div>
             </div>
+            
           </div>
         </div>
 
