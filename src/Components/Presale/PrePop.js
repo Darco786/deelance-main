2c805890ac2ab22356f2d25b8e724bff32af035c
draco <122390064+Darco786@users.noreply.github.com>
2023-02-06 17:45:26 +0530

    new

--- src/Components/Presale/PrePop.js
+++ src/Components/Presale/PrePop.js
@@ -6,11 +6,12 @@ import { ethers } from "ethers";
 import { TokenList } from "../../Constants/Constants";
 
 function PrePop({ setIsModal }) {
-  const { connectWallet, provider, contracts, account } = useContext(UserContext);
+  const { connectWallet, provider, contracts, account } =
+    useContext(UserContext);
   const [balances, setBalances] = useState({ BNB: 0 });
   const [showComp, setShowComp] = useState(false);
   const tokenElement = useRef({ value: "" });
-  const nftAmountElement = useRef()
+  const nftAmountElement = useRef();
   const [selectedToken, setSelectedToken] = useState("BNB");
   const maxa = useRef();
   const [secondInputValue, setSecondInputValue] = useState(0);
@@ -31,17 +32,14 @@ function PrePop({ setIsModal }) {
         USDT: 0,
       });
 
-   tokenElement.current = { value: "" };
-
+      tokenElement.current = { value: "" };
     } else {
-
       tokenElement.current = { value: "" };
 
       const getBNBBalance = async () => {
         const balance = await provider.getBalance(account);
         return ethers.utils.formatEther(balance);
       };
-
 
       const getTokenBalances = async (token) => {
         console.log(token, " getting balance");
@@ -133,39 +131,36 @@ function PrePop({ setIsModal }) {
 
   const handleTokenChange = (e) => {
     setSelectedToken(e.target.value);
-  }
+  };
 
-  const handleMaxClick =  async (e) => {
+  const handleMaxClick = async (e) => {
     e.preventDefault();
-    console.log("PROVA",selectedToken)
-   if (selectedToken === "BNB") {
-      maxa.current.value = Math.max((balances.BNB));
-
-
+    console.log("PROVA", selectedToken);
+    if (selectedToken === "BNB") {
+      maxa.current.value = Math.max(balances.BNB);
     } else {
-      maxa.current.value = Math.max((balances["USDT"]));
-
+      maxa.current.value = Math.max(balances["USDT"]);
     }
   };
-  
+
   const handleFirstInputChange = async (event) => {
     if (selectedToken === "USDT") {
-    const inputValue = event.target.value;
-    const pri = await contracts.Main.salePrice();
-    const prezzo = ethers.utils.formatEther(pri);
-    const priBNB =  parseInt(inputValue / prezzo, 10);
-    console.log(priBNB)
-    setSecondInputValue(priBNB);
+      const inputValue = event.target.value;
+      const pri = await contracts.Main.salePrice();
+      const prezzo = ethers.utils.formatEther(pri);
+      const priBNB = parseInt(inputValue / prezzo, 10);
+      console.log(priBNB);
+      setSecondInputValue(priBNB);
     } else {
-    const inputValue = event.target.value;
-    const pri = await contracts.Main.salePrice();
-    const prezzo = ethers.utils.formatEther(pri);
-    const prezzo_BNB = await contracts.Main.getBNBLatestPrice();
-    const prezzo_BNB_formattato = ethers.utils.formatEther(prezzo_BNB);
-    const mio_valore = inputValue * prezzo_BNB_formattato;
-    const finale = parseInt(mio_valore / prezzo, 10);
-    console.log(finale)
-    setSecondInputValue(finale);
+      const inputValue = event.target.value;
+      const pri = await contracts.Main.salePrice();
+      const prezzo = ethers.utils.formatEther(pri);
+      const prezzo_BNB = await contracts.Main.getBNBLatestPrice();
+      const prezzo_BNB_formattato = ethers.utils.formatEther(prezzo_BNB);
+      const mio_valore = inputValue * prezzo_BNB_formattato;
+      const finale = parseInt(mio_valore / prezzo, 10);
+      console.log(finale);
+      setSecondInputValue(finale);
     }
   };
 
@@ -182,55 +177,62 @@ function PrePop({ setIsModal }) {
           </button>
         </div>
         <div className="">
-          <section className="section-box">
+          <section className="">
             <div className="formWrapper-2">
               <header className="pop-pre-h">
-              <h2 className=" mb-10px lh-1">
-  Balance: {balances[selectedToken]} {selectedToken}
-</h2>
+                <h2 className=" mb-10px lh-1">
+                  Balance: {balances[selectedToken]} {selectedToken}
+                </h2>
               </header>
               <div className="d-flex pre-step-1">
-                <h3>Step-1: You Pay</h3>
+                <h3>1. YOU PAY</h3>
                 <div className="select-coin">
-                <select id="option" ref={tokenElement} style={{background:'#00e069'}} onChange={handleTokenChange}>
-  <option value="BNB">BNB</option>
-  <option value="USDT">USDT</option>
-</select>
+                  <select
+                    id="option"
+                    ref={tokenElement}
+                    style={{ background: "#00e069",height:'24px' }}
+                    onChange={handleTokenChange}
+                  >
+                    <option value="BNB">BNB</option>
+                    <option value="USDT">USDT</option>
+                  </select>
+                </div>
+                <span className="white">:</span>
+                <div className="d-flex">
+                  <form className="dee-bal">
+                    <input
+                      type="number"
+                      className="fs-26px  weight-3 in-put-pre"
+                      placeholder="0.00"
+                      onChange={handleFirstInputChange}
+                      ref={maxa}
+                      required
+                    />
 
+                    <button onClick={handleMaxClick} className='btn-max'>MAX</button>
+                  </form>
                 </div>
               </div>
-              <div className="d-flex">
-                <form className="dee-bal">
+
+              <div className="d-flex pre-step-1">
+                <h3>2. YOU GET</h3>
+                <div className="dee-coin green">$dlance</div>
+                <span className="white">:</span>
+
+                <div className="">
                   <input
                     type="number"
-                    className="fs-26px  weight-3 in-put-pre"
+                    className="fs-26px  weight-3 in-put-pre wt-100 "
                     placeholder="0.00"
-                    onChange={handleFirstInputChange}
-                    ref={maxa}
+                    value={secondInputValue}
+                    ref={nftAmountElement}
                     required
                   />
+                </div>
+              </div>
 
-<button onClick={handleMaxClick}>MAX</button>
-                </form>
-              </div>
-              <div className="d-flex pre-step-1">
-                <h3>Step-2: You Get</h3>
-                <div className="dee-coin white">$dlance</div>
-              </div>
-              <div className="">
-                <input
-                  type="number"
-                  className="fs-26px  weight-3 in-put-pre wt-100 "
-                  placeholder="0.00"
-                  value={secondInputValue}
-                  ref={nftAmountElement}
-                  required
-                />
-               
-              </div>
-              <div className="mt-auto">
-                
-              <button type="submit" className="pre-pop-btn"  onClick={buyNFT}>
+              <div className="m-auto text-center">
+                <button type="submit" className="pre-pop-btn" onClick={buyNFT}>
                   Buy Now
                 </button>
               </div>
