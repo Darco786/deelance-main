07e908b974500c1839635863bdb3599440218257
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-02-15 15:57:03 +0500

    changed nothing, formatted the code

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Academy from 'Pages/Academy';
-import HowBuy from 'Pages/HowBuy';
-import JobPortal from 'Pages/JobPortal';
-import NftMarket from 'Pages/NftMarket';
-import Privacy from 'Pages/Privacy';
-import Reward from 'Pages/Reward';
-import Risk from 'Pages/Risk';
-import TeamPage from 'Pages/TeamPage';
-import Terms from 'Pages/Terms';
-import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
-import Home from './Pages/Home'
-import NotFound from './Pages/404Page'
-import { ethers } from 'ethers';
-import { useState } from 'react';
-import Web3Modal from 'web3modal';
-import { BEP20ABI, BigNFTABI } from './Constants/ABI';
-import { ContractAddr, providerOptions, RPCUrl } from './Constants/Constants';
-import UserContext from './UserContext';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Academy from "Pages/Academy";
+import HowBuy from "Pages/HowBuy";
+import JobPortal from "Pages/JobPortal";
+import NftMarket from "Pages/NftMarket";
+import Privacy from "Pages/Privacy";
+import Reward from "Pages/Reward";
+import Risk from "Pages/Risk";
+import TeamPage from "Pages/TeamPage";
+import Terms from "Pages/Terms";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/404Page";
+import { ethers } from "ethers";
+import { useState } from "react";
+import Web3Modal from "web3modal";
+import { BEP20ABI, BigNFTABI } from "./Constants/ABI";
+import { ContractAddr, providerOptions, RPCUrl } from "./Constants/Constants";
+import UserContext from "./UserContext";
 
 const web3Modal = new Web3Modal({
   cacheProvider: true, // optional
   providerOptions, // required
 });
 
-
 function App() {
   const defaultProvider = new ethers.providers.JsonRpcProvider(RPCUrl);
-  const readContract = new ethers.Contract(ContractAddr.Main, BigNFTABI, defaultProvider);
+  const readContract = new ethers.Contract(
+    ContractAddr.Main,
+    BigNFTABI,
+    defaultProvider
+  );
 
   const [provider, setProvider] = useState(defaultProvider);
   const [account, setAccount] = useState();
   const [contracts, setContracts] = useState({
-    Main: readContract
+    Main: readContract,
   });
   const [connectError, setConnectError] = useState("");
 
   const connectWallet = async () => {
     if (account) {
-      return
+      return;
     }
 
     try {
@@ -50,20 +52,23 @@ function App() {
       }
       provider = new ethers.providers.Web3Provider(provider);
 
-      const contracts = {}
+      const contracts = {};
       for (const [token, address] of Object.entries(ContractAddr)) {
-        contracts[token] = new ethers.Contract(address, token == "Main" ? BigNFTABI : BEP20ABI, provider.getSigner())
+        contracts[token] = new ethers.Contract(
+          address,
+          token == "Main" ? BigNFTABI : BEP20ABI,
+          provider.getSigner()
+        );
       }
       setContracts(contracts);
       setProvider(provider);
 
       const accounts = await provider.listAccounts();
-      if (accounts)
-        setAccount(accounts[0]);
+      if (accounts) setAccount(accounts[0]);
       return true;
     } catch (error) {
-      if (error !== 'Modal closed by user') {
-        setConnectError(error)
+      if (error !== "Modal closed by user") {
+        setConnectError(error);
       }
       return false;
     }
@@ -75,25 +80,33 @@ function App() {
     setConnectError("");
   };
   return (
-    < >
-      <UserContext.Provider value={{ provider, account, contracts, connectError, connectWallet, disconnectWallet }}>
+    <>
+      <UserContext.Provider
+        value={{
+          provider,
+          account,
+          contracts,
+          connectError,
+          connectWallet,
+          disconnectWallet,
+        }}
+      >
         <Router>
-        <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route exact path="/nft-market" element={<NftMarket/>}/>
-          <Route exact path="/job-portal" element={<JobPortal/>}/>
-          <Route exact path="/academy" element={<Academy/>}/>
-          <Route exact path="/team" element={<TeamPage/>}/>
-          <Route exact path="/privacy-policy" element={<Privacy/>}/>
-          <Route exact path="/terms" element={<Terms/>}/>
-          <Route exact path="/risk" element={<Risk/>}/>
-          <Route exact path="/rewards" element={<Reward/>}/>
-          <Route exact path="/how-to-buy" element={<HowBuy/>}/>
-          <Route exact path="*" element={<NotFound/>}/>
-        
-        </Routes>
-    </Router>
-    </UserContext.Provider>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/nft-market" element={<NftMarket />} />
+            <Route exact path="/job-portal" element={<JobPortal />} />
+            <Route exact path="/academy" element={<Academy />} />
+            <Route exact path="/team" element={<TeamPage />} />
+            <Route exact path="/privacy-policy" element={<Privacy />} />
+            <Route exact path="/terms" element={<Terms />} />
+            <Route exact path="/risk" element={<Risk />} />
+            <Route exact path="/rewards" element={<Reward />} />
+            <Route exact path="/how-to-buy" element={<HowBuy />} />
+            <Route exact path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </UserContext.Provider>
     </>
   );
 }
