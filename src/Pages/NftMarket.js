b9c4e0d5f279ea5e549fbe0f76004f386f49ede4
draco <122390064+Darco786@users.noreply.github.com>
2023-02-28 01:08:40 +0530

    meta data updated

--- src/Pages/NftMarket.js
+++ src/Pages/NftMarket.js
@@ -5,11 +5,26 @@ import React from "react";
 import Navbar from "Components/Navbar/Navbar";
 import Footer from "Components/Footer/Footer";
 import Warning from "Components/Common/Warning";
-
+import { Helmet } from "react-helmet";
 function NftMarket() {
   return (
     <>
       
+<Helmet>
+        <meta
+          name="NFT Marketplace"
+          content="Explore exclusive NFT’s by the most in-demand creators, trade with other collectors in the Marketplace, and mint your own NFTs all in one place."
+        />
+        <meta
+          name="keywords"
+          content="Freelance, fiver , upwork , jobs , NFT , content , marketing, freelancers, works , metaverse, web3, Binance,Deelance"
+        />
+        <meta
+          name="description"
+          content="Explore exclusive NFT’s by the most in-demand creators, trade with other collectors in the Marketplace, and mint your own NFTs all in one place."
+        />
+        <title>NFT Marketplace</title>
+      </Helmet>
         
        
        <Navbar/>
--- src/Pages/Reward.js
+++ src/Pages/Reward.js
@@ -1,20 +1,41 @@
-import Warning from 'Components/Common/Warning'
-import Footer from 'Components/Footer/Footer'
-import Navbar from 'Components/Navbar/Navbar'
-import Rewards from 'Components/Rewards/Rewards'
-import React from 'react'
+import Warning from "Components/Common/Warning";
+import Footer from "Components/Footer/Footer";
+import Navbar from "Components/Navbar/Navbar";
+import Rewards from "Components/Rewards/Rewards";
+import React from "react";
+import { Helmet } from "react-helmet";
 
 function Reward() {
   return (
     <>
-   
-       <Navbar/>
-        <Rewards/>
 
-<Warning/>
-    <Footer/>
+<Helmet>
+        <meta
+          name="Win100k"
+          content="Participate in giveaway and win $100,000  worth of $Dlance"
+        />
+        <meta
+          name="keywords"
+          content="Freelance, fiver , upwork , jobs , NFT , content , marketing, freelancers, works , metaverse, web3, Binance,Deelance,
+    freelancer,upwork freelancer,Fiver,freelancer website,freelancer affiliate program,freelancer accounting jobs,freelancer app developer,account freelancer,best freelancer sites,become a freelancer on fiverr,freelancer customer service,freelance content writer,canva freelancer,freelancer digital marketer ,freelancer earning, 
+jobs,indeed jobs, 
+jobs near me, 
+jobs hiring near me,work from home jobs,jobs available near me,nft marketplace,nft marketplace list,nft marketplace ranking,nft marketplace crypto,nft marketplace github,nft marketplace website,nft marketplace aggregator,nft marketplace as a service,metaverse,metaverse meaning,facebook metaverse,mark zuckerberg metaverse,sandbox metaverse,how to invest in metaverse,metaverse game
+,digital marketing,blockchain developer"
+        />
+        <meta
+          name="description"
+          content="All you have to do is follow the steps described in the widget on this page to receive multiple entries, and hold at least $100 of $Dlance at the time of the draw."
+        />
+        <title>Win100k</title>
+      </Helmet>
+      <Navbar />
+      <Rewards />
+
+      <Warning />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Reward
+export default Reward;
